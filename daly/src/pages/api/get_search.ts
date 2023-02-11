f55836cb39a47e8fd025232aaa832e0ae186408f
dav <usr21p1@gmail.com>
2023-02-11 14:45:53 -0800

    Change rapid apis search to google

--- daly/src/pages/api/get_search.ts
+++ daly/src/pages/api/get_search.ts
@@ -3,40 +3,41 @@ import { reverse } from "dns";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 interface Item {
-    link: string;
-    htmlTitle: string;
-    displayLink: string;
-    htmlSnippet: string;
+  link: string;
+  htmlTitle: string;
+  displayLink: string;
+  htmlSnippet: string;
 }
 
 type Data = {
-    ok: boolean;
-    items: [Item];
+  items: [Item];
 };
 
 export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
 ) {
-    (async () => {
-        let phrase = req.body.searchText;
-        let searchRes = await get_search(phrase);
-        res.status(200).json({
-            ok: searchRes.status == "success",
-            items: searchRes.items,
-        });
-    })();
+  (async () => {
+    let phrase = req.body.searchText;
+    let searchRes = await get_search(phrase);
+    res.status(200).json({
+      items: searchRes.items,
+    });
+  })();
 }
 
 async function get_search(phrase: string): Promise<any> {
-    let url = new URL("https://google-search72.p.rapidapi.com/search");
-    url.searchParams.append("query", phrase);
-    let fetchRes = await fetch(url, {
-        headers: {
-            "X-RapidAPI-Host": "google-search72.p.rapidapi.com",
-            "X-RapidAPI-Key": process.env.RAPID_API_KEY,
-        },
-    });
-    let data = await fetchRes.json();
-    return data;
+  let url = new URL("https://www.googleapis.com/customsearch/v1");
+  url.searchParams.append("q", phrase);
+  url.searchParams.append("key", process.env.SEARCH_API_KEY as string);
+  url.searchParams.append("cx", process.env.SEARCH_CX as string);
+  let fetchRes = await fetch(url, {
+    headers: {
+      // "X-RapidAPI-Host": "google-search72.p.rapidapi.com",
+      // "X-RapidAPI-Key": process.env.RAPID_API_KEY as string,
+    },
+  });
+  let data = await fetchRes.json();
+  console.log(data);
+  return data;
 }
