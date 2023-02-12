c7a75c0f446221566f18fd0282cd0bfa39411d48
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2023-02-11 18:20:02 -0800

    fixing stuff

--- daly/src/pages/Homepage/Homepage.tsx
+++ daly/src/pages/Homepage/Homepage.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Inter } from '@next/font/google'
 import { Oswald } from '@next/font/google'
+import Link from 'next/link'
+import NextLink from "next/link"
+import { useRouter } from 'next/router'
 
 const oswald = Oswald({
     weight: '700',
@@ -8,19 +11,22 @@ const oswald = Oswald({
 })
 
 const Homepage = () => {
+
   return (
+    <NextLink href="/">
     <div>
         <div className={oswald.className}>
-            <h1 className='flex text-8xl justify-center drop-shadow-xl'>
+            <div className='flex text-8xl justify-center drop-shadow-xl' onClick={() => window.location.reload()}>
                 <div className=' text-green-600'  >E</div>
                 <div className=' text-red-600'>L</div>
                 <div className=' text-orange-500'>G</div>
                 <div className=' text-purple-600'>O</div>
                 <div className='text-green-600'>O</div>
                 <div className='text-orange-500'>G</div>
-            </h1>
+            </div>
         </div>
     </div>
+    </NextLink>
   )
 }
 
