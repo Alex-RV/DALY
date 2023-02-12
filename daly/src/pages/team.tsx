47ba78f721593267ee768f31a10acbc1f376d6f4
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-02-11 19:18:14 -0800

    Update team.tsx

--- daly/src/pages/team.tsx
+++ daly/src/pages/team.tsx
@@ -1,15 +1,53 @@
 import Container from 'components/Container'
 import React from 'react'
+import Image from 'next/image'
 
 export default function team() {
   return (
     <Container>
-        <div className="flex flex-col items-start max-w-3xl mx-auto my-16">
-        <div className='flex flex-row sm:mb-1 mb-5'>
-          <div className='flex flex-col h-full '>
-            <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-            Our Team
+        <div className="flex flex-col items-start max-w-3xl mx-auto my-36">
+        <div className='flex flex-col  sm:mb-1 mb-5'>
+          <div className='flex flex-col '>
+            <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-16 text-black dark:text-white">
+            Our Team:
             </h1>
+          </div>
+          <div className='grid gap-10 flex-row grid-cols-2'>
+          <Image
+            alt="photo"
+            height={300}
+            width={300}
+            src="/screen.png"
+            sizes="30vw"
+            priority
+            className="rounded-full filter mb-5"
+          />
+          <Image
+            alt="photo"
+            height={300}
+            width={300}
+            src="/screen.png"
+            sizes="30vw"
+            priority
+            className="rounded-full filter mb-5"
+          />
+          <Image
+            alt="photo"
+            height={300}
+            width={300}
+            src="/screen.png"
+            sizes="30vw"
+            priority
+            className="rounded-full filter mb-16"
+          /><Image
+          alt="photo"
+          height={300}
+          width={300}
+          src="/screen.png"
+          sizes="30vw"
+          priority
+          className="rounded-full filter mb-16"
+        />
           </div>
           </div>
           </div>
