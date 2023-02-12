cf78d3ea20cc05c7a7f0dd13597ab39f3e1ecfd8
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-02-11 18:37:30 -0800

    created team and changed about

--- daly/src/pages/about.tsx
+++ daly/src/pages/about.tsx
@@ -8,9 +8,9 @@ export default function about() {
     <div>
         <div className='bg-[#f9fafb] p-20 dark:bg-[#131415] pb-52'>
             <div className='flex flex-column justify-center text-center dark:text-[#f9fafb]'>
-                <div>
-                <h1 className='text-8xl my-4'>About</h1>   
-                <p className=' text-4xl my-4'>El goog is a search engine dedicated to getting the wrong answers right</p> 
+                <div className="font-bold  tracking-tight mb-4 text-black dark:text-white">
+                <h1 className='text-5xl md:text-7xl my-4'>About</h1>   
+                <p className='text-2xl md:text-4xl my-4'>El goog is a search engine dedicated to getting the wrong answers right</p> 
                 </div>
             </div>
         </div> 
--- daly/src/pages/team.tsx
+++ daly/src/pages/team.tsx
@@ -1,7 +1,19 @@
+import Container from 'components/Container'
 import React from 'react'
 
 export default function team() {
   return (
-    <div>team</div>
+    <Container>
+        <div className="flex flex-col items-start max-w-3xl mx-auto my-16">
+        <div className='flex flex-row sm:mb-1 mb-5'>
+          <div className='flex flex-col h-full '>
+            <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+            Our Team
+            </h1>
+          </div>
+          </div>
+          </div>
+    </Container>
+    
   )
 }
